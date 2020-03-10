1176779e79c6671916333d64d1e01636ebc195bf
Gluppe <Magnusgluppe97@gmail.com>
2020-03-10 11:26:47 +0100

    Created a HashMap

--- src/main/java/gruppe2/imagingapplication/Image.java
+++ src/main/java/gruppe2/imagingapplication/Image.java
@@ -2,6 +2,11 @@ package gruppe2.imagingapplication;
 
 import java.util.HashMap;
 
+/**
+ * This is a HashMap that contains the metadata of the images
+ * There is a unique HashMap for each image and the different keys
+ * are all the different metadata of the image
+ */
 public class Image {
     HashMap<String, String> path = new HashMap<>();
 }
