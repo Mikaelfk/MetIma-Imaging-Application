54a123297ac1431cfe62af6a54042b9322060887
Mikkel Aas <aa.mikkel2902@gmail.com>
2020-03-10 15:09:01 +0100

    Added java documentation to AddImagePageController.java

--- src/main/java/gruppe2/imagingapplication/gui/AddImagePageController.java
+++ src/main/java/gruppe2/imagingapplication/gui/AddImagePageController.java
@@ -17,21 +17,37 @@ public class AddImagePageController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
+    /**
+     * This method handles what happens when the home button is pressed
+     * @param event
+     */
     @FXML
     private void btnHome(ActionEvent event){
         logger.info("Home button pressed.");
     }
 
+    /**
+     * This method handles what happens when the add button is pressed
+     * @param event Represents the action when the button is pressed
+     */
     @FXML
     private void btnAdd(ActionEvent event){
         logger.info("Add button pressed.");
     }
 
+    /**
+     * This method handles what happens when the cancel button is pressed
+     * @param event Represents the action when the button is pressed
+     */
     @FXML
     private void btnCancel(ActionEvent event){
         logger.info("Cancel button pressed.");
     }
 
+    /**
+     * This method handles what happens when the gallery button is pressed
+     * @param event Represents the action when the button is pressed
+     */
     @FXML
     private void btnGallery(ActionEvent event){
         logger.info("Gallery button pressed.");
