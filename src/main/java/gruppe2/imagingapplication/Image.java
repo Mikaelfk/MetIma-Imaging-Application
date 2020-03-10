8d0ad7ce0d90cf3659569632b8524b8494a46abb
Mikael Falkenberg Krog <mikaelfk@stud.ntnu.no>
2020-03-10 11:47:43 +0100

    Changed fields, setters and getters for the Image class

--- src/main/java/gruppe2/imagingapplication/Image.java
+++ src/main/java/gruppe2/imagingapplication/Image.java
@@ -6,13 +6,14 @@ import java.util.HashMap;
 public class Image {
     private String filename;
     private String tags;
+    private String path;
 
     /**
      * This is a HashMap that contains the metadata of the images
      * There is a unique HashMap for each image and the different keys
      * are all the different metadata of the image
      */
-    private HashMap<String, String> path = new HashMap<>();
+    private HashMap<String, String> metadata = new HashMap<>();
 
     public String getFilename() {
         return filename;
@@ -30,11 +31,19 @@ public class Image {
         this.tags = tags;
     }
 
-    public HashMap<String, String> getPath() {
+    public String getPath() {
         return path;
     }
 
-    public void setPath(HashMap<String, String> path) {
+    public void setPath(String path) {
         this.path = path;
+    }
+
+    public HashMap<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(HashMap<String, String> metadata) {
+        this.metadata = metadata;
     }
 }
