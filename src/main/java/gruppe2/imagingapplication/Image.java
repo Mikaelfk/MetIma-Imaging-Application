88e2d1de3ee809d18f2be7389dcab2fba264ea70
Gluppe <Magnusgluppe97@gmail.com>
2020-03-10 11:42:10 +0100

    JavaDoc

--- src/main/java/gruppe2/imagingapplication/Image.java
+++ src/main/java/gruppe2/imagingapplication/Image.java
@@ -2,13 +2,15 @@ package gruppe2.imagingapplication;
 
 import java.util.HashMap;
 
-/**
- * This is a HashMap that contains the metadata of the images
- * There is a unique HashMap for each image and the different keys
- * are all the different metadata of the image
- */
+
 public class Image {
     private String filename;
     private String tags;
+
+    /**
+     * This is a HashMap that contains the metadata of the images
+     * There is a unique HashMap for each image and the different keys
+     * are all the different metadata of the image
+     */
     private HashMap<String, String> path = new HashMap<>();
 }
