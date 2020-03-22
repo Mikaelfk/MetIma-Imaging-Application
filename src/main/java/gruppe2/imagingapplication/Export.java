91082ccc9db231959925909063a5cabe5ff31f82
Mikkel Aas <aa.mikkel2902@gmail.com>
2020-03-22 12:07:54 +0000

    Added a logger to Export.java Fixed a bug where the export function creates a empty pdf document when there are no images in the gallery.

--- src/main/java/gruppe2/imagingapplication/Export.java
+++ src/main/java/gruppe2/imagingapplication/Export.java
@@ -7,9 +7,12 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class Export {
+  Logger logger = LoggerFactory.getLogger(Export.class);
   /**
    * This method generates a pdf document and adds images to it based on the HashMap you give it
    *
@@ -19,32 +22,37 @@ public class Export {
    */
   public void exportImagesToPdf(HashMap<String, ImageData> images,
                                 String filename) throws IOException {
-    PDDocument document = new PDDocument();
+    if (images.isEmpty()) {
+        logger.info("No pictures.");
+    } else {
 
-    images.forEach((String key, ImageData image) -> {
-      String path = image.getPath();
-      PDPage page = new PDPage();
-      document.addPage(page);
-      try {
-        PDPageContentStream contentStream = new PDPageContentStream(document, page);
-        PDImageXObject imageXObject = PDImageXObject.createFromFile(path, document);
-        double height = 0;
-        double width = 0;
-        if(imageXObject.getWidth() <= imageXObject.getHeight()) {
-          width = 600*((double)imageXObject.getWidth()/imageXObject.getHeight());
-          height = 600;
-          contentStream.drawImage(imageXObject, 0, 0, (float)width, (float)height);
-        } else {
-          height = 600*((double)imageXObject.getHeight()/imageXObject.getWidth());
-          width = 600;
-          contentStream.drawImage(imageXObject, 6, 300, (float)width, (float)height);
+      PDDocument document = new PDDocument();
+
+      images.forEach((String key, ImageData image) -> {
+        String path = image.getPath();
+        PDPage page = new PDPage();
+        document.addPage(page);
+        try {
+          PDPageContentStream contentStream = new PDPageContentStream(document, page);
+          PDImageXObject imageXObject = PDImageXObject.createFromFile(path, document);
+          double height = 0;
+          double width = 0;
+          if (imageXObject.getWidth() <= imageXObject.getHeight()) {
+            width = 600 * ((double) imageXObject.getWidth() / imageXObject.getHeight());
+            height = 600;
+            contentStream.drawImage(imageXObject, 0, 0, (float) width, (float) height);
+          } else {
+            height = 600 * ((double) imageXObject.getHeight() / imageXObject.getWidth());
+            width = 600;
+            contentStream.drawImage(imageXObject, 6, 300, (float) width, (float) height);
+          }
+          contentStream.close();
+
+        } catch (IOException exception) {
+          logger.error("No images found", exception);
         }
-        contentStream.close();
-
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    });
-    document.save(filename + ".pdf");
+      });
+      document.save(filename + ".pdf");
+    }
   }
 }
