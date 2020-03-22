af6253814e918ce39c8069ca7e0c45f44f1484b3
Mikael Falkenberg Krog <mikaelfk@stud.ntnu.no>
2020-03-22 12:39:30 +0100

    Added try-with-resources block

--- src/main/java/gruppe2/imagingapplication/Export.java
+++ src/main/java/gruppe2/imagingapplication/Export.java
@@ -26,39 +26,40 @@ public class Export {
     if (images.isEmpty()) {
         logger.info("No pictures.");
     } else {
+      try (PDDocument document = new PDDocument()) {
+        images.forEach((String key, ImageData image) -> {
+          String path = image.getPath();
+          PDPage page = new PDPage();
+          document.addPage(page);
+          try {
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            PDImageXObject imageXObject = PDImageXObject.createFromFile(path, document);
+            double height;
+            double width;
+            if (imageXObject.getWidth() <= imageXObject.getHeight()) {
+              width = 600 * ((double) imageXObject.getWidth() / imageXObject.getHeight());
+              height = 600;
+              contentStream.drawImage(imageXObject, 0, 0, (float) width, (float) height);
+            } else {
+              height = 600 * ((double) imageXObject.getHeight() / imageXObject.getWidth());
+              width = 600;
+              contentStream.drawImage(imageXObject, 6, 300, (float) width, (float) height);
+            }
+            contentStream.close();
 
-      PDDocument document = new PDDocument();
-
-      images.forEach((String key, ImageData image) -> {
-        String path = image.getPath();
-        PDPage page = new PDPage();
-        document.addPage(page);
-        try {
-          PDPageContentStream contentStream = new PDPageContentStream(document, page);
-          PDImageXObject imageXObject = PDImageXObject.createFromFile(path, document);
-          double height;
-          double width;
-          if (imageXObject.getWidth() <= imageXObject.getHeight()) {
-            width = 600 * ((double) imageXObject.getWidth() / imageXObject.getHeight());
-            height = 600;
-            contentStream.drawImage(imageXObject, 0, 0, (float) width, (float) height);
-          } else {
-            height = 600 * ((double) imageXObject.getHeight() / imageXObject.getWidth());
-            width = 600;
-            contentStream.drawImage(imageXObject, 6, 300, (float) width, (float) height);
+          } catch (IOException exception) {
+            logger.error("No images found", exception);
           }
-          contentStream.close();
-
-        } catch (IOException exception) {
-          logger.error("No images found", exception);
+        });
+        FileChooser fileChooser = new FileChooser();
+        File saveLocation = fileChooser.showSaveDialog(new Stage());
+        if (!saveLocation.getAbsolutePath().endsWith(".pdf")) {
+          document.save(saveLocation.getAbsolutePath() + ".pdf");
+        } else {
+          document.save(saveLocation.getAbsolutePath());
         }
-      });
-      FileChooser fileChooser = new FileChooser();
-      File saveLocation = fileChooser.showSaveDialog(new Stage());
-      if (!saveLocation.getAbsolutePath().endsWith(".pdf")) {
-        document.save(saveLocation.getAbsolutePath() + ".pdf");
-      } else {
-        document.save(saveLocation.getAbsolutePath());
+      } catch(Exception e) {
+        logger.error("Could not export to pdf");
       }
     }
   }
