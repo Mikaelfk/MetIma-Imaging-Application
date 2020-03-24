ab62579ded00607fd089aecaa6499f579c44afb7
Jakob Karlsmoen <jakob.karlsmoen@hotmail.no>
2020-03-24 21:41:25 +0100

    Pages in exported PDF are now properly scaled and all images are sized based on A4 paper width

--- src/main/java/gruppe2/imagingapplication/Export.java
+++ src/main/java/gruppe2/imagingapplication/Export.java
@@ -1,76 +1,91 @@
 package gruppe2.imagingapplication;
 
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.Map;
+import javafx.print.Paper;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 public class Export {
   Logger logger = LoggerFactory.getLogger(Export.class);
 
   /**
    * This method generates a pdf document and adds images to it based on the HashMap you give it.
+   * Width of image in pdf will be standard A4 width, and height is fixed to image ratio.
    *
-   * @param images Takes a HashMap<String, ImageData>
-   *               that holds the images you wish to export to a pdf document
+   * @param images Takes a Map that holds the images you wish to export to a pdf document
    */
-  public void exportImagesToPdf(HashMap<String, ImageData> images) {
-    if (images.isEmpty()) {
-      logger.info("No pictures.");
-    } else {
-      try (PDDocument document = new PDDocument()) {
-        images.forEach((String key, ImageData image) -> {
-          String path = image.getPath();
-          PDPage page = new PDPage();
-          document.addPage(page);
-          try {
-            PDPageContentStream contentStream = new PDPageContentStream(document, page);
-            PDImageXObject imageXObject = PDImageXObject.createFromFile(path, document);
-            double height;
-            double width;
-            if (imageXObject.getWidth() <= imageXObject.getHeight()) {
-              width = 600 * ((double) imageXObject.getWidth() / imageXObject.getHeight());
-              height = 600;
-              contentStream.drawImage(imageXObject, 0, 0, (float) width, (float) height);
-            } else {
-              height = 600 * ((double) imageXObject.getHeight() / imageXObject.getWidth());
-              width = 600;
-              contentStream.drawImage(imageXObject, 6, 300, (float) width, (float) height);
-            }
-            contentStream.close();
+  public void exportImagesToPdf(Map<String, ImageData> images) {
+    Document document = new Document();
 
-          } catch (IOException exception) {
-            logger.error("No images found", exception);
-          }
-        });
-        saveDocument(document);
-      } catch (Exception e) {
-        logger.error("Could not export to pdf");
-      }
+    try {
+      logger.info("Requesting save location for export");
+      PdfWriter.getInstance(document, new FileOutputStream(getSaveLocation()));
+    } catch (IOException e) {
+      logger.info("User didn't choose a save location, cancelling save export operation...");
+      return;
+    } catch (DocumentException e) {
+      logger.error("Error occured when trying to write to file", e);
     }
+
+    document.open();
+
+    images.keySet().forEach(path -> {
+      Image image = null;
+      try {
+        image = Image.getInstance("file:" + path);
+      } catch (BadElementException e) {
+        logger.error("Error creating image object", e);
+      } catch (IOException e) {
+        logger.error("Error accessing image file", e);
+      }
+
+      // calculate page size / ratio based on standard A4 page
+      Rectangle size = new Rectangle((float) Paper.A4.getWidth(),
+          (float) Paper.A4.getWidth() * (image.getPlainHeight() / image.getPlainWidth()));
+      document.setPageSize(size);
+
+      // set image properties and position
+      image.setAbsolutePosition(0,0);
+      image.setBorderWidth(0);
+      image.scaleToFit(size);
+
+      // create new page and add image to said page
+      document.newPage();
+      try {
+        document.add(image);
+      } catch (DocumentException e) {
+        // this should never happen :-P
+        logger.error("Document is not opened", e);
+      }
+    });
+    document.close();
   }
 
   /**
-   * Takes a PDDocument and saves it with a file chooser.
-   *
-   * @param document Takes a PDDocument
-   * @throws IOException Throws an exception if the document does not exist
+   * Method for getting a path to where the user wants to save the exported document.
+   * @return The absolute path where the pdf will be saved
    */
-  public void saveDocument(PDDocument document) throws IOException {
+  public String getSaveLocation() throws IOException {
     FileChooser fileChooser = new FileChooser();
     File saveLocation = fileChooser.showSaveDialog(new Stage());
+
+    // throw exception if no save location was chosen
+    if (saveLocation == null) {
+      throw new IOException("User cancelled during save location selection");
+    }
+
+    // checks if the user included file extension when selecting save location
     if (!saveLocation.getAbsolutePath().endsWith(".pdf")) {
-      document.save(saveLocation.getAbsolutePath() + ".pdf");
+      return (saveLocation.getAbsolutePath() + ".pdf");
     } else {
-      document.save(saveLocation.getAbsolutePath());
+      return (saveLocation.getAbsolutePath());
     }
   }
 }
