5cf11d12ebbd1bbe78e56ce8d24ee2bb1f205e2b
Gluppe <Magnusgluppe97@gmail.com>
2020-03-30 18:11:31 +0200

    Added a method for collecting tags to a single String in ImageData

--- src/main/java/gruppe2/imagingapplication/ImageData.java
+++ src/main/java/gruppe2/imagingapplication/ImageData.java
@@ -4,13 +4,16 @@ package gruppe2.imagingapplication;
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Directory;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 import com.drew.metadata.Tag;
 import javafx.scene.image.Image;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
@@ -20,33 +23,32 @@ import javax.persistence.Transient;
  * A class representing an image with metadata and tags.
  */
 @Entity
-public class ImageData implements Serializable{
+public class ImageData implements Serializable {
   @Id
   private String path;
   private String imageName;
-  @Transient
-  private List<String> tags;
+  private String tags;
   private String metadata;
   @Transient
   private Image image;
 
   /**
    * This is a constructor for the Image class.
+   *
    * @param absolutePath The absolute path to the image for the class to represent
    * @param tags         User-defined tags to describe image, set null for no tags
    * @throws ImageProcessingException If filetype is unknown
    * @throws IOException              If file could not be found
    */
   public ImageData(String absolutePath, List<String> tags, Image image) throws
-          ImageProcessingException, IOException
-  {
+      ImageProcessingException, IOException {
     this.path = absolutePath;
     this.metadata = convertMetadataToString();
     this.image = image;
     if (tags != null) {
-      this.tags = tags;
+      this.tags = convertTagsToString(tags);
     } else {
-      this.tags = new ArrayList<>();
+      this.tags = "";
     }
   }
 
@@ -56,9 +58,10 @@ public class ImageData implements Serializable{
 
   /**
    * This method converts the metadata into a listed String.
+   *
    * @return Returns the metadata as a listed String
    * @throws ImageProcessingException If filetype is unknown
-   * @throws IOException If file could not be found
+   * @throws IOException              If file could not be found
    */
   public String convertMetadataToString() throws ImageProcessingException, IOException {
     StringBuilder metaData = new StringBuilder();
@@ -70,6 +73,19 @@ public class ImageData implements Serializable{
     return metaData.toString();
   }
 
+  /**
+   * This method collects all the tags to a single string.
+   * @param tags A list of tags
+   * @return returns the String of tags
+   */
+  public String convertTagsToString(List<String> tags) {
+    StringBuilder allTheTags = new StringBuilder();
+    for(String tag: tags) {
+      allTheTags.append(tag).append("\n");
+    }
+    return allTheTags.toString();
+  }
+
   public String getImageName() {
     return imageName;
   }
@@ -78,13 +94,13 @@ public class ImageData implements Serializable{
     this.imageName = imageName;
   }
 
-  public List<String> getTags() {
+  public String getTags() {
     return tags;
   }
 
-  public void addTag(String tag) {
+  /*public void addTag(String tag) {
     this.tags.add(tag);
-  }
+  }*/
 
   public String getPath() {
     return path;
