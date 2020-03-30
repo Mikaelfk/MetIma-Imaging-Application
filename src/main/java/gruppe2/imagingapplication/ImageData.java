489323e474ca08213c3ddadf6c3cbf5622770e73
Mikael Falkenberg Krog <mikaelfk@stud.ntnu.no>
2020-03-30 18:30:31 +0200

    Revert "Changed Metadata field to String"
    This reverts commit cfe4850f

--- src/main/java/gruppe2/imagingapplication/ImageData.java
+++ src/main/java/gruppe2/imagingapplication/ImageData.java
@@ -3,31 +3,22 @@ package gruppe2.imagingapplication;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
 import java.io.File;
 import java.io.IOException;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import com.drew.metadata.Tag;
 import javafx.scene.image.Image;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Transient;
 
 
 /**
  * A class representing an image with metadata and tags.
  */
-@Entity
-public class ImageData implements Serializable{
-  @Id
-  private String path;
+public class ImageData {
   private String imageName;
-  @Transient
   private List<String> tags;
-  private String metadata;
-  @Transient
+  private String path;
+  private Metadata metadata;
   private Image image;
 
   /**
@@ -38,26 +29,15 @@ public class ImageData implements Serializable{
    * @throws IOException              If file could not be found
    */
   public ImageData(String absolutePath, List<String> tags, Image image) throws
-          ImageProcessingException, IOException
-  {
+          ImageProcessingException, IOException {
     this.path = absolutePath;
-    StringBuilder metaData = new StringBuilder();
-    for (Directory directory : ImageMetadataReader.readMetadata(new File(path)).getDirectories()) {
-      for (Tag tag : directory.getTags()) {
-        metaData.append(tag.toString()).append("\n");
-      }
-    }
-    this.metadata = metaData.toString();
+    this.metadata = ImageMetadataReader.readMetadata(new File(path));
     this.image = image;
     if (tags != null) {
       this.tags = tags;
     } else {
       this.tags = new ArrayList<>();
     }
-  }
-
-  public ImageData() {
-
   }
 
   public String getImageName() {
@@ -84,7 +64,7 @@ public class ImageData implements Serializable{
     this.path = path;
   }
 
-  public String getMetadata() {
+  public Metadata getMetadata() {
     return metadata;
   }
 
