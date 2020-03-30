b8dfe8a81b11b7127caf6094959fc71823e29002
Jakob Karlsmoen <jakob.karlsmoen@hotmail.no>
2020-03-30 17:15:12 +0200

    Revert "Restructured for two different add image pages depending on if user wants to add a single image or many at once"
    This reverts commit 96dced0b

--- src/main/java/gruppe2/imagingapplication/gui/AddImagePageController.java
+++ src/main/java/gruppe2/imagingapplication/gui/AddImagePageController.java
@@ -21,7 +21,7 @@ import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AddSingleImagePageController implements Initializable {
+public class AddImagePageController implements Initializable {
 
   @FXML
   private Text selectedFileText;
@@ -35,8 +35,8 @@ public class AddSingleImagePageController implements Initializable {
   private Button btnAddImage;
   private boolean imageSelected = false;
   private File selectedFile;
-  private List<File> multipleFiles;
-  Logger logger = LoggerFactory.getLogger(AddSingleImagePageController.class);
+  private List<File> MultipleFiles;
+  Logger logger = LoggerFactory.getLogger(AddImagePageController.class);
 
 
   @Override
@@ -57,8 +57,8 @@ public class AddSingleImagePageController implements Initializable {
   @FXML
   private void chooseFile(ActionEvent event) {
     FileChooser fileChooser = new FileChooser();
-    multipleFiles = fileChooser.showOpenMultipleDialog(new Stage());
-    for (File selectedImage : multipleFiles) {
+    MultipleFiles = fileChooser.showOpenMultipleDialog(new Stage());
+    for (File selectedImage : MultipleFiles) {
       if (selectedImage != null) {
         selectedFileText.setText(selectedImage.getName());
         imagePreview.setImage(new Image("file:" + selectedImage.getAbsolutePath()));
@@ -79,13 +79,12 @@ public class AddSingleImagePageController implements Initializable {
     if (!txtTags.getText().isEmpty()) {
       tags = Arrays.asList(txtTags.getText().split("\\s*,\\s*"));
     }
-    for (File selectedImage : multipleFiles) {
+    for (File selectedImage : MultipleFiles) {
       MetImaApplication.getContentManager().addImageToDB(selectedImage.getAbsolutePath(), tags);
       MetImaApplication.getContentManager().getImages().get(selectedImage.getAbsolutePath())
           .setImageName(txtFileName.getText());
     }
-    MetImaApplication.getScene().setRoot(
-            FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
+    MetImaApplication.getScene().setRoot(FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
   }
 
   /**
@@ -97,8 +96,7 @@ public class AddSingleImagePageController implements Initializable {
   @FXML
   private void btnHome(ActionEvent event) {
     try {
-      MetImaApplication.getScene().setRoot(
-              FXMLLoader.load(getClass().getResource("MetIma_HomePage.fxml")));
+      MetImaApplication.getScene().setRoot(FXMLLoader.load(getClass().getResource("MetIma_HomePage.fxml")));
     } catch (IOException exception) {
       logger.error("File not found", exception);
     }
@@ -113,8 +111,7 @@ public class AddSingleImagePageController implements Initializable {
   @FXML
   private void btnGallery(ActionEvent event) {
     try {
-      MetImaApplication.getScene().setRoot(
-              FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
+      MetImaApplication.getScene().setRoot(FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
     } catch (IOException exception) {
       logger.error("File not found", exception);
     }
