1a0ae4717af1245cef5efae7ef97b886eec63b1a
Jakob Karlsmoen <jakob.karlsmoen@hotmail.no>
2020-03-31 12:48:39 +0200

    AddImagePage refactored to support multiple adding multiple images at once

--- src/main/java/gruppe2/imagingapplication/gui/AddImagePageController.java
+++ src/main/java/gruppe2/imagingapplication/gui/AddImagePageController.java
@@ -3,88 +3,89 @@ package gruppe2.imagingapplication.gui;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
-import javafx.beans.binding.Bindings;
+import java.util.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.text.Text;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.imageio.ImageIO;
 
 public class AddImagePageController implements Initializable {
+  private Map<Integer, File> chosenImages = new HashMap<>();
+  private Logger logger = LoggerFactory.getLogger(AddImagePageController.class);
 
   @FXML
-  private Text selectedFileText;
-  @FXML
-  private ImageView imagePreview;
-  @FXML
-  private TextField txtFileName;
-  @FXML
-  private TextField txtTags;
-  @FXML
-  private Button btnAddImage;
-  private boolean imageSelected = false;
-  private File selectedFile;
-  private List<File> MultipleFiles;
-  Logger logger = LoggerFactory.getLogger(AddImagePageController.class);
+  private VBox entryContainer;
 
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    btnAddImage.disableProperty().bind(Bindings.createBooleanBinding(
-        () -> txtFileName.getText().trim().isEmpty() || !imageSelected,
-        txtFileName.textProperty(), btnAddImage.defaultButtonProperty()
-    ));
-  }
-
-  /**
-   * This method prompts the user tp chooses a file from your library and
-   * opens the picture in the image preview.
-   *
-   * @param event Selecting an image in your library
-   */
-  @FXML
-  private void chooseFile(ActionEvent event) {
     FileChooser fileChooser = new FileChooser();
-    MultipleFiles = fileChooser.showOpenMultipleDialog(new Stage());
-    for (File selectedImage : MultipleFiles) {
-      if (selectedImage != null) {
-        selectedFileText.setText(selectedImage.getName());
-        imagePreview.setImage(new Image("file:" + selectedImage.getAbsolutePath()));
-        if (!imagePreview.getImage().isError()) {
-          imageSelected = true;
+
+    // let user choose files
+    List<File> chosenFiles = fileChooser.showOpenMultipleDialog(new Stage());
+
+    // check for image files and associate image file with an identifier in chosenImages HashMap
+    int identifier = 0;
+    for (File file : chosenFiles) {
+      try {
+        // try to read image file
+        if (ImageIO.read(new File(file.getAbsolutePath())) != null) {
+          chosenImages.put(identifier, file);
+          identifier++;
+        } else {
+          logger.info("File is not an image ({})", file.getName());
         }
+      } catch (IOException e) {
+        logger.error("File doesn't exist", e);
       }
     }
+
+    // create an entry for all images in HashMap
+    // TODO: do something if no images were chosen
+    chosenImages.forEach((imageIdentifier, imageFile) -> {
+      entryContainer.getChildren()
+          .add(createListEntry(imageFile.getAbsolutePath(), imageIdentifier));
+    });
   }
 
   /**
-   * This method the the chosen file and ads it to the gallery.
-   * Then it takes you to the gallery page.
+   * Method for creating an HBox that represents the entries of chosen images.
+   * The method changes node fx:id to associate entry with and image and it's identifier,
+   * and configures image for the preview.
+   * @param imagePath The filepath of the image
+   * @param identifier The identifier for keeping track of images and entries
+   * @return Generated HBox
    */
-  @FXML
-  private void addImage(ActionEvent event) throws IOException {
-    List<String> tags = null;
-    if (!txtTags.getText().isEmpty()) {
-      tags = Arrays.asList(txtTags.getText().split("\\s*,\\s*"));
+  private HBox createListEntry(String imagePath, int identifier) {
+    HBox entry = null;
+
+    // load fxml for HBox
+    try {
+      entry = FXMLLoader.load(getClass().getResource("AddImagePage_Entry.fxml"));
+    } catch (IOException exception) {
+      logger.error("File not found", exception);
     }
-    for (File selectedImage : MultipleFiles) {
-      MetImaApplication.getContentManager().addImageToDB(selectedImage.getAbsolutePath(), tags);
-      MetImaApplication.getContentManager().getImages().get(selectedImage.getAbsolutePath())
-          .setImageName(txtFileName.getText());
-    }
-    MetImaApplication.getScene().setRoot(FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
+
+    // get nodes by default id and add identifier at the end for later reference
+    entry.setId("entry" + identifier);
+    entry.lookup("#txtEntryName").setId("txtEntryName" + identifier);
+    entry.lookup("#txtEntryTags").setId("txtEntryTags" + identifier);
+    entry.lookup("#btnEntryRemove").setId("btnEntryRemove" + identifier);
+
+    // set image for preview
+    ImageView previewImage = (ImageView) entry.lookup("#imgEntryPreview");
+    previewImage.setImage(new Image("file:" + imagePath));
+
+    return entry;
   }
 
   /**
@@ -96,7 +97,9 @@ public class AddImagePageController implements Initializable {
   @FXML
   private void btnHome(ActionEvent event) {
     try {
-      MetImaApplication.getScene().setRoot(FXMLLoader.load(getClass().getResource("MetIma_HomePage.fxml")));
+      MetImaApplication.getScene()
+          .setRoot(FXMLLoader.load(getClass()
+              .getResource("MetIma_HomePage.fxml")));
     } catch (IOException exception) {
       logger.error("File not found", exception);
     }
@@ -111,7 +114,9 @@ public class AddImagePageController implements Initializable {
   @FXML
   private void btnGallery(ActionEvent event) {
     try {
-      MetImaApplication.getScene().setRoot(FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
+      MetImaApplication.getScene()
+          .setRoot(FXMLLoader.load(getClass()
+              .getResource("MetIma_GalleryPage.fxml")));
     } catch (IOException exception) {
       logger.error("File not found", exception);
     }
