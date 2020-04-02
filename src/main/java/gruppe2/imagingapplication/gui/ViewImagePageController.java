9644d56ca87f46b8209e922f661a9a8fd67ee612
Gluppe <Magnusgluppe97@gmail.com>
2020-04-02 17:41:06 +0200

    Added a confirmation dialog to the delete button in view image

--- src/main/java/gruppe2/imagingapplication/gui/ViewImagePageController.java
+++ src/main/java/gruppe2/imagingapplication/gui/ViewImagePageController.java
@@ -6,11 +6,14 @@ import gruppe2.imagingapplication.ImageData;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -103,13 +106,28 @@ public class ViewImagePageController implements Initializable {
    */
   @FXML
   private void btnDelete() {
-    MetImaApplication.getContentManager().removeImage(this.image.getPath());
-    try {
-      MetImaApplication.getScene().setRoot(
-          FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
-    } catch (IOException exception) {
-      logger.error(FILE_NOT_FOUND, exception);
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Confirmation Dialog");
+    alert.setHeaderText("You are trying to delete an image");
+    alert.setContentText("Are you sure you wish to delete?");
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.get() == ButtonType.OK) {
+      MetImaApplication.getContentManager().removeImage(this.image.getPath());
+      try {
+        MetImaApplication.getScene().setRoot(
+            FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
+      } catch (IOException exception) {
+        logger.error(FILE_NOT_FOUND, exception);
+      }
+    } else {
+      try {
+        MetImaApplication.getScene().setRoot(
+            FXMLLoader.load(getClass().getResource("MetIma_GalleryPage.fxml")));
+      } catch (IOException exception) {
+        logger.error(FILE_NOT_FOUND, exception);
+      }
     }
+
   }
 
   private void btnEdit() {
