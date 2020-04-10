65aff9d0e601ee5c177aa16eb645a5aaab39a78e
Gluppe <Magnusgluppe97@gmail.com>
2020-04-10 13:00:40 +0200

    Made an ContentManagerTest class

--- src/main/java/gruppe2/imagingapplication/gui/ViewImagePageController.java
+++ src/main/java/gruppe2/imagingapplication/gui/ViewImagePageController.java
@@ -1,14 +1,12 @@
 package gruppe2.imagingapplication.gui;
 
 import gruppe2.imagingapplication.ImageData;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
---
+++ src/test/java/ContentManagerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import gruppe2.imagingapplication.ImageData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ContentManagerTest {
+
+  @BeforeEach
+  void setUp(){
+
+  }
+}
