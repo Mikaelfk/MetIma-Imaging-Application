f13b24484ac49f87341538ae8ed41eb9c70dfdc3
Jakob Karlsmoen <jakob.karlsmoen@hotmail.no>
2020-04-16 23:19:03 +0200

    Added test for image tags

--- src/test/java/gruppe2/imagingapplication/ImageDataTest.java
+++ src/test/java/gruppe2/imagingapplication/ImageDataTest.java
@@ -2,8 +2,14 @@ package gruppe2.imagingapplication;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ImageDataTest {
   private Logger logger = LoggerFactory.getLogger(ImageDataTest.class);
@@ -26,12 +32,31 @@ public class ImageDataTest {
     }
 
     logger.info("Test 1: Hashmap is populated with data");
-    if (testImage.getMetadata().isEmpty()) {
-      // hashmap is empty, which means no metadata was found
-      assert false;
-    } else {
-      // hashmap is empty, which means metadata was found
-      assert true;
+    // checks if hashmap has any data inside
+    assertFalse(testImage.getMetadata().isEmpty());
+  }
+
+  @Test
+  public void tagTests() {
+    ImageData testImage = null;
+    List<String> tags = Arrays.asList(new String[]{"tags", "are", "cool"});
+
+    try {
+      // need test imge to work with, this path might be different depending on current working directory
+      // works if working directory is project root
+      testImage = new ImageData(System.getProperty("user.dir")
+          + "/src/test/java/gruppe2/imagingapplication/test_image.png", tags, "test_image");
+    } catch (Exception e) {
+      logger.error("Image file not found", e);
     }
+
+    logger.info("Test 2: Object has correct amount of tags");
+    assertTrue(testImage.getTags().size() == 3);
+
+    logger.info("Test 3: Object has correct tags");
+    assertTrue(testImage.getTags().contains("tags")
+        && testImage.getTags().contains("are")
+        && testImage.getTags().contains("cool")
+    );
   }
 }
